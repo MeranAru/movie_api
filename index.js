b9a8b21617f192b9f7163d948b7923640fa806ff
Meran Arumugam <meran.arumugam@gmail.com>
2023-05-15 15:10:24 -0400

    Update index.js
    Fixed coding errors and added CRUD

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require('express'),
-    app = express();
-    morgan = require('morgan'),
-    bodyParser = required('bodyParser'),
-    uuid = required('uuid');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
     //import built in node modules fs and path
-    fs = require('fs'),
-    path = require('path');
+const fs = require('fs');
+const path = require('path');
     // create a write stream(in append mode) 
     //log text file is created in root directory
-const { Stream } = require('stream');
-    const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+const app = express();
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
     
 
     //send all files automatically to the public folder
@@ -22,50 +21,50 @@ let users = [
     {
         id: 1,
         name: 'Steve',
-        favoriteMovie: []
+        favoriteMovies: []
     },
     {
         id: 2,
         name: 'Joe',
-        facoriteMovies: []
+        favoriteMovies: ['Avengers:Endgame']
     },
 ]
 
-let topMovies = [
+let movies = [
     {
-        title: 'Fast and the Furious',
-        description: 'A police officer decides where his loyalty belongs when he is sent to street racing world to be undercover to destroy.',
-        genre: {
-            name: 'Action',
-            description: 'In the movie industry, action movies is when a protaganist is sent into numerous events which have violence.',
+        Title: 'Fast and the Furious',
+        Description: 'A police officer decides where his loyalty belongs when he is sent to street racing world to be undercover to destroy.',
+        Genre: {
+            Name: 'Action',
+            Description: 'In the movie industry, action movies is when a protaganist is sent into numerous events which have violence.',
             },
-        director: {
-            name: 'Rob Cohen',
-            born: 'March 12, 1949',
-            bio: 'Rob Cohen is an American film director, producer and screenwriter.'
+        Director: {
+            Name: 'Rob Cohen',
+            Born: 'March 12, 1949',
+            Bio: 'Rob Cohen is an American film director, producer and screenwriter.'
         },
     },
     {
-        title: 'Avengers: Endgame',
-        description: 'Avengers assemble once more to defeat Thanos and to restore the Universe and balance to the world.',
-        genre: {
-            name: 'Action',
-            description: 'In the movie industry, action movies is when a protaganist is sent into numerous events which have violence.',
+        Title: 'Avengers:Endgame',
+        Description: 'Avengers assemble once more to defeat Thanos and to restore the Universe and balance to the world.',
+        Genre: {
+            Name: 'Action',
+            Description: 'In the movie industry, action movies is when a protaganist is sent into numerous events which have violence.',
             },
-        director: {
-            name: 'Anthony Russo',
-            born: 'February 3, 1970',
-            bio: 'Anthony J. Russo is an American filmmaker and producer who works alongside his brother Joseph Russo.'
+        Director: {
+            Name: 'Anthony Russo',
+            Born: 'February 3, 1970',
+            Bio: 'Anthony J. Russo is an American filmmaker and producer who works alongside his brother Joseph Russo.'
             },
     },
     {
-        title: 'Spider-Man: No way Home',
-        description: 'Spider man has got his identity revealed and got help from Doctor Strange. A spell went wrong and dangerous enemies from other worlds start to appear where Peter learns what it means to be Spider-Man.',
-        genre: {
-            name: 'Action',
-            description: 'In the movie industry, action movies is when a protaganist is sent into numerous events which have violence.',
+        Title: 'Spider-Man: No way Home',
+        Description: 'Spider man has got his identity revealed and got help from Doctor Strange. A spell went wrong and dangerous enemies from other worlds start to appear where Peter learns what it means to be Spider-Man.',
+        Genre: {
+            Name: 'Action',
+            Description: 'In the movie industry, action movies is when a protaganist is sent into numerous events which have violence.',
             },
-        director: {
+        Director: {
             name: 'Jon Watts',
             born: 'June 28, 1981',
             bio: 'Jon Watts is an American filmmaker and screenwriter.'
@@ -82,10 +81,106 @@ app.get('/documentation', (req,res) => {
     res.sendFile('public/documentation.html', { root: __dirname});
 });
 
-app.get('/movies', (req,res) => {
-    res.json(topMovies);
-});
+//Create
+app.post('/users', (req,res) =>{
+    const newUser = req.body;
 
+    if (newUser.name) {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(200).json(newUser)
+    } else {
+        res.status(400).send('users need names')
+    }
+})
+//Update
+app.put('/users/:id', (req,res) =>{
+    const { id } = req.params;
+    const updatedUser = req.body;
+
+    let user = users.find( user => user.id == id);
+    
+    if (user) {
+        user.name = updatedUser.name;
+        res.status(200).json(user)
+    } else {
+        res.status(400).send('no such user');
+    }
+})
+//Create
+app.post('/users/:id/:movieTitle', (req,res) =>{
+    const { id, movieTitle } = req.params;
+
+    let user = users.find( user => user.id == id);
+
+    if (user) {
+        user.favoriteMovies.push(movieTitle);
+        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+    } else {
+        res.status(400).send('no such user');
+    }
+})
+//Delete
+app.delete('/users/:id/:movieTitle', (req,res) =>{
+    const { id, movieTitle } = req.params;
+
+    let user = users.find( user => user.id == id);
+    if (user) {
+        user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+    } else {
+        res.status(400).send('no such user');
+    }
+})
+//Delete
+app.delete('/users/:id', (req,res) =>{
+    const { id } = req.params;
+
+    let user = users.find( user => user.id == id);
+    if (user) {
+        user = users.find( user => user.id != id);
+        res.status(200).send(`user ${id}'s has been deleted`);
+    } else {
+        res.status(400).send('no such user');
+    }
+})
+//Read
+app.get('/movies', (req,res) => {
+    res.status(200).json(movies);
+})
+//Read
+app.get('/movies/:title', (req,res) => {
+    const { title } = req.params;
+    const movie = movies.find( movie => movie.Title === title );
+
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(400).send('no such movie')
+    }
+})
+//Read
+app.get('/movies/genre/:genreName', (req,res) => {
+    const { genreName } = req.params;
+    const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
+
+    if (genre) {
+        res.status(200).json(genre);
+    } else {
+        res.status(400).send('no such genre')
+    }
+})
+//Read
+app.get('/movies/director/:directorName', (req,res) => {
+    const { directorName } = req.params;
+    const director = movies.find( movie => movie.Director.Name === directorName ).Director;
+
+    if (director) {
+        res.status(200).json(director);
+    } else {
+        res.status(400).send('no such director')
+    }
+})
 //Error code that detects errors above
 app.use((err, req, res, next) => {
     console.error(err.stack);
